7d6f768a112767bf881e140197d7ad65927a8ad8
Shintaro Kurihara <kuri.creep.kuri@gmail.com>
2020-07-06 17:52:43 +0900

    save

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -9,6 +9,15 @@
       </ion-toolbar>
     </ion-header>
     <ion-content padding>
+
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" v-for="(each, index) in photos" :key="index">
+            <ion-img :src="each.base64 ? each.base64 : each.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
       <ion-fab vertical="bottom" horizontal="end" slot="fixed">
         <ion-fab-button @click="takePhoto()">
           <ion-icon name="camera" />
@@ -20,27 +29,119 @@
 </template>
 
 <script>
+
 import {
   Plugins,
   CameraSource,
   CameraResultType,
+  FilesystemDirectory,
+  Storage,
+  Capacitor,
 } from '@capacitor/core';
 
-const { Camera } = Plugins;
+function isPlatform(identify) {
+  return identify === 'hybrid' && Capacitor.platform === 'web';
+}
+
+const { Camera, Filesystem } = Plugins;
+
+async function readAsBase64(image) {
+  if (!isPlatform('hybrid')) {
+    return Promise.resolve(async () => {
+      const file = await Filesystem.readFile({
+        path: image.filePath,
+      });
+      return file.data;
+    });
+  }
+
+  const response = await fetch(image.webPath);
+  const blob = await response.blob();
+  const reader = new FileReader();
+  return new Promise((resolve, reject) => {
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+
+    reader.readAsDataURL(blob);
+  });
+}
+
+const PHOTO_STORAGE = 'photos';
 
 export default {
   name: 'NewItemPage',
+  data() {
+    return {
+      photos: [],
+    };
+  },
+  async mounted() {
+    // Retrieve cached photo array data
+    const photos = await Storage.get({ key: PHOTO_STORAGE });
+    const geted = JSON.parse(photos.value) || [];
+
+    if (isPlatform('hybrid')) {
+      geted.forEach(async (each) => {
+        const photo = each;
+        const readFile = await Filesystem.readFile({
+          path: photo.filePath,
+          directory: FilesystemDirectory.Data,
+        });
+        photo.base64 = `data:image/jpeg;base64,${readFile.data}`;
+        this.photos.push(photo);
+      });
+    } else {
+      this.photos.push(...geted);
+    }
+  },
   methods: {
-    takePhoto: async () => {
-      /* const image = */ await Camera.getPhoto({
+    async takePhoto() {
+      const image = await Camera.getPhoto({
         quality: 90,
         allowEditing: true,
-        resultType: CameraResultType.DataUrl,
-        source: CameraSource.Prompt,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+      });
+
+      const filePath = `${new Date().getTime()}.jpeg`;
+      const base64 = await readAsBase64(image);
+      const savedFile = await Filesystem.writeFile({
+        path: filePath,
+        data: base64,
+        directory: FilesystemDirectory.Data,
+      });
+
+      if (!isPlatform('hybrid')) {
+        this.photos.push({
+          filePath: savedFile.uri,
+          webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+          base64,
+        });
+      } else {
+        this.photos.push({
+          filePath,
+          webviewPath: image.webPath,
+          base64,
+        });
+      }
+
+      Storage.set({
+        key: PHOTO_STORAGE,
+        value: isPlatform('hybrid')
+          ? JSON.stringify(this.photos)
+          : JSON.stringify(this.photos.map((p) => {
+          // Don't save the base64 representation of the photo data,
+          // since it's already saved on the Filesystem
+            const photoCopy = { ...p };
+            delete photoCopy.base64;
+
+            return photoCopy;
+          })),
       });
     },
   },
-
 };
 </script>
 
